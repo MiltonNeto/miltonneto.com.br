707e6797de95eb94219ae3f90d09603183b3ac30
MiltonNeto <leonardi.milton@gmail.com>
2023-01-02 20:22:00 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import styles from '../styles/css/Home.module.css'
+
+export default function Home() {  
+  return (
+    
+        <div id="appBody">
+            <header>
+                <h1>Header</h1>
+            </header>
+            <main>
+                <h1>Content</h1>
+            </main>
+            <footer>
+                <h1>footer</h1>
+            </footer>
+
+        </div>
+
+  )
+}
