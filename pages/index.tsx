e24419979f14dc41ce03047b9dc32900c1474d32
Milton Neto <leonardi.milton@gmail.com>
2023-01-10 23:03:13 -0300

    10012023 2303

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ export default function Home() {
       <meta name="description" content="Portfolio Digital de Milton Neto, Designer de interfaces digitais e especialista em experiência de usuário. Chega aí, vamos bater um papo sobre seu projeto!"/>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
     </Head>
 
     <main>
