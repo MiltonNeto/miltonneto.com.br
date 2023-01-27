108fe5f6a5488dbddddd9057f0ef982bf3d48bfc
MiltonNeto <leonardi.milton@gmail.com>
2023-01-26 22:47:45 -0300

    Gtag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import Head from 'next/head'
 import { Container } from 'reactstrap'
 import Image from 'next/image'
@@ -17,9 +18,27 @@ export default function Home() {
       <meta name="description" content="Portfolio Digital de Milton Neto, Designer de interfaces digitais e especialista em experiência de usuário. Chega aí, vamos bater um papo sobre seu projeto!"/>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
+      
     </Head>
       <>
         <div className='main'>
+
+        <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+        strategy="afterInteractive"
+        />
+         <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-D4FGK6BR9K');
+        `}
+        </Script>
+
+
+
           <Header />
           
           <Container>
