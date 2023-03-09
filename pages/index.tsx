b276c5c3fb13a31ebf676de7e47f2e56483d87c0
MiltonNeto <leonardi.milton@gmail.com>
2023-03-09 15:06:00 -0300

    instalação do clarity

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,13 @@ export default function Home() {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
       <link rel="icon" href="/favicon.ico" />
+      <script type="text/javascript">
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "fklssfdh1j");
+      </script>
       
     </Head>
       <>
