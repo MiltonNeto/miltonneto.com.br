d94acb5199906f29647beeb997d4688071766acf
Milton Neto <leonardi.milton@gmail.com>
2023-03-18 17:41:09 -0300

    atualizacoes do magnificourier

--- pages/ProjectMagnifiCourier.tsx
+++ pages/ProjectMagnifiCourier.tsx
@@ -56,15 +56,8 @@ export default function ProjectMagnifiCourier(){
             <h4 className={styles.sectionTitle}>Usuários</h4>
                 <Col xxl="12" xs="12">
                     <p>
-                        Foram realizadas entrevistas com diferentes perfis de usuários, que responderam às seguintes perguntas:
+                        Foram realizadas entrevistas com diferentes perfis de usuários, que responderam a algumas perguntas referente ao uso de serviços de entrega e o quanto isso facilitaria no seu cotidiano.
                     </p>
-                    <ul className={styles.sectionTopicList}>
-                        <li>Já utilizou serviço de motoboy ou entrega?</li>
-                        <li>Utilizaria entrega através de um aplicativo?</li>
-                        <li>O que mais pesaria na escolha utilizar do app?</li>
-                        <li>O que se espera ver num app de entrega?</li>
-                        <li>Aceitaria o uso de entrega compartilhada no caso de essa entrega demorar um pouco mais para ser realizada?</li>
-                    </ul>
                     <div className="quote">
                         Destaco aqui, como exemplo, duas personas que participaram destas pesquisas. E uma terceira, que participou como usuário do que seria a versão DRIVER do MagnifiCourier.
                     </div>
@@ -95,18 +88,21 @@ export default function ProjectMagnifiCourier(){
                             </li>
                         </ul>
                     </Col>
-                    <Col xxl="9" className={styles.personaAnswers}>
-                        <ul className={styles.sectionTopicList}>
-                            <li>Já utilizou serviço de motoboy ou entrega?</li>
-                                <li>Sim, certamente</li>
-                            <li>Utilizaria entrega através de um aplicativo?</li>
-                                <li>Sim, pra mim facilitaria muito</li>
-                            <li>O que mais pesaria na escolha utilizar do app?</li>
-                                <li>Ter dinamismo: agilidade, opções de entrega, cotações, informações claras e bem definidas ( tempo e  opções de escolha de veículo a entregar) em tempo real e centralizadas em um único lugar</li>
-                            <li>O que se espera ver num app de entrega?</li>
-                                <li>Ponto de saída, Ponto de entrega, Peso, Dimensões, Tipos de entrega, Confirmação de recebimento</li>
-                            <li>Aceitaria o uso de entrega compartilhada no caso de essa entrega demorar um pouco mais para ser realizada?</li>
-                                <li>Sim, pois certamente o valor seria mais barato e, por não trabalhar com produtos perecíveis ou cargas vivas, não teria tanta pressa e poderia planejar e  aguardar.</li>
+                    <Col xxl="9" className={styles.personaSurvey}>
+                        <ul>
+                            <li className={styles.personaQuestion}>Já utilizou serviço de motoboy ou entrega?</li>
+                                <li className={styles.personaAnswer}>Sim, certamente</li>
+                            <li className={styles.personaQuestion}>Utilizaria entrega através de um aplicativo?</li>
+                                <li className={styles.personaAnswer}>Sim, pra mim facilitaria muito</li>
+                            <li className={styles.personaQuestion}>O que mais pesaria na escolha utilizar do app?</li>
+                                <li className={styles.personaAnswer}>Ter dinamismo: agilidade, opções de entrega, cotações, informações claras e bem definidas (tempo e  opções de escolha de veículo a entregar) em tempo real e centralizadas em um único lugar</li>
+                            <li className={styles.personaQuestion}>O que se espera ver num app de entrega?</li>
+                                <li className={styles.personaAnswer}>Ponto de saída, ponto de entrega, tipos de entrega, confirmação de recebimento, de repente seria legal se soubesse o tamanho ou o peso do pacote que fosse receber, tem coisas que não dá pra carregar sozinha.</li>
+                            <li className={styles.personaQuestion}>Aceitaria o uso de entrega compartilhada no caso de essa entrega demorar um pouco mais para ser realizada?</li>
+                                <li className={styles.personaAnswer}>Sim, pois certamente o valor seria mais barato e, por não trabalhar com produtos perecíveis ou cargas vivas, não teria tanta pressa e poderia planejar e  aguardar.</li>
+
+                            <li className={styles.personaQuestion}>Acha que é possível uma empresa se preocupar com meio ambiente trabalhando com meios de transporte, que são poluentes de um modo geral?</li>
+                                <li className={styles.personaAnswer}>Se é possível, não consigo imaginar como, mas acho muito importante uma empresa seja qual for, se preocupar com meio ambiente, ter medidas internas talvez de coleta seletiva ou economia de papel, qualquer medida de preservação ambiental é bem vista.</li>
                         </ul>
                     </Col>
                 </Row>
@@ -114,39 +110,41 @@ export default function ProjectMagnifiCourier(){
                 <Row className={styles.cardPersona}>
                     <Col xxl="3" className={styles.personaProfile}>
                         <Image src="/images/projetos/magnificourier/persona-mauricio.png" width='200' height='200' alt="persona" className={styles.personaImage} />
-                        <p className={styles.personaQuote}>"...por vezes me falta algum material, de produção e não posso sair para buscar, e ao mesmo tempo tenho prazos para entrega. Ao poonto que consigo me programar, algumas vezes preciso de algo com mais urgência. Imprevistos acontecem!"
+                        <p className={styles.personaQuote}>"Infelizmente alguns documentos ainda precisam ser assinados fisicamente, com essa história de pandemia, por mais que isso tenha agilizado a digitalização de muitos serviços, muita coisa ainda não é possível"
                         </p>
 
                         <ul className={styles.personaInfo}>
                             <li>
                                 <label>Nome</label>
                                 <br/>
-                                Daniela
+                                Alex
                             </li>
                             <li>
                                 <label>Idade</label>
                                 <br/>
-                                33 anos
+                                43 anos
                             </li>
                             <li> 
                                 <label>Ocupação</label>
                                 <br/>
-                                Profissional autônoma
+                                Gerente administrativo
                             </li>
                         </ul>
                     </Col>
-                    <Col xxl="9" className={styles.personaAnswers}>
-                        <ul className={styles.sectionTopicList}>
-                            <li>Já utilizou serviço de motoboy ou entrega?</li>
-                                <li>Sim, certamente</li>
-                            <li>Utilizaria entrega através de um aplicativo?</li>
-                                <li>Sim, pra mim facilitaria muito</li>
-                            <li>O que mais pesaria na escolha utilizar do app?</li>
-                                <li>Ter dinamismo: agilidade, opções de entrega, cotações, informações claras e bem definidas ( tempo e  opções de escolha de veículo a entregar) em tempo real e centralizadas em um único lugar</li>
-                            <li>O que se espera ver num app de entrega?</li>
-                                <li>Ponto de saída, Ponto de entrega, Peso, Dimensões, Tipos de entrega, Confirmação de recebimento</li>
-                            <li>Aceitaria o uso de entrega compartilhada no caso de essa entrega demorar um pouco mais para ser realizada?</li>
-                                <li>Sim, pois certamente o valor seria mais barato e, por não trabalhar com produtos perecíveis ou cargas vivas, não teria tanta pressa e poderia planejar e  aguardar.</li>
+                    <Col xxl="9" className={styles.personaSurvey}>
+                        <ul>
+                            <li className={styles.personaQuestion}>Já utilizou serviço de motoboy ou entrega?</li>
+                                <li className={styles.personaAnswer}>Sim, mais do que deveria inclusive</li>
+                            <li className={styles.personaQuestion}>Utilizaria entrega através de um aplicativo?</li>
+                                <li className={styles.personaAnswer}>Já utilizo vários, de comida principalmente</li>
+                            <li className={styles.personaQuestion}>O que mais pesaria na escolha utilizar do app?</li>
+                                <li className={styles.personaAnswer}>Não precisar procurar o menor preço e saber o horário que minha entrega será feita</li>
+                            <li className={styles.personaQuestion}>O que se espera ver num app de entrega?</li>
+                                <li className={styles.personaAnswer}>Custo, Previsão de entrega, Confirmação de recebimento;</li>
+                            <li className={styles.personaQuestion}>Aceitaria o uso de entrega compartilhada no caso de essa entrega demorar um pouco mais para ser realizada?</li>
+                                <li className={styles.personaAnswer}>Dependeria muito da minha urgência, às vezes preciso de uma entrega mais urgente, ou com horário marcado, hoje já temos serviços de documentos online, mas ainda assim algumas coisas precisam ser assinadas fisicamente e essas precisam ser mais rápidas.</li>
+                                <li className={styles.personaQuestion}>Acha que é possível uma empresa se preocupar com meio ambiente trabalhando com meios de transporte, que são poluentes de um modo geral?</li>
+                                    <li className={styles.personaAnswer}>Parece difícil né. Mas sei lá, seria importante, aqui na empresa a gente tem energia solar, é pouco, mas é um começo agora, com veículos parece mais utópico. Veículos elétricos talvez.</li>
                         </ul>
                     </Col>
                 </Row>
@@ -154,45 +152,114 @@ export default function ProjectMagnifiCourier(){
                 <Row className={styles.cardPersona}>
                     <Col xxl="3" className={styles.personaProfile}>
                     <Image src="/images/projetos/magnificourier/persona-rodrigo.png" width='200' height='200' alt="persona" className={styles.personaImage} />
-                    <p className={styles.personaQuote}>"...por vezes me falta algum material, de produção e não posso sair para buscar, e ao mesmo tempo tenho prazos para entrega. Ao poonto que consigo me programar, algumas vezes preciso de algo com mais urgência. Imprevistos acontecem!"
+                    <p className={styles.personaQuote}>"Quanto mais entregas eu puder fazer, mais eu ganho, se eu puder economizar na gasolina e na manutenção da moto, melhor ainda. Poder programar mais de uma entrega por vez então seria bem legal, fazer uma rota só com várias entregas."
                         </p>
 
                         <ul className={styles.personaInfo}>
                             <li>
                                 <label>Nome</label>
                                 <br/>
-                                Daniela
+                                Rodrigo
                             </li>
                             <li>
                                 <label>Idade</label>
                                 <br/>
-                                33 anos
+                                39 anos
                             </li>
                             <li> 
                                 <label>Ocupação</label>
                                 <br/>
-                                Profissional autônoma
+                                Motoboy
                             </li>
                         </ul>
                     </Col>
-                    <Col xxl="9" className={styles.personaAnswers}>
-                        <ul className={styles.sectionTopicList}>
-                            <li>Já utilizou serviço de motoboy ou entrega?</li>
-                                <li>Sim, certamente</li>
-                            <li>Utilizaria entrega através de um aplicativo?</li>
-                                <li>Sim, pra mim facilitaria muito</li>
-                            <li>O que mais pesaria na escolha utilizar do app?</li>
-                                <li>Ter dinamismo: agilidade, opções de entrega, cotações, informações claras e bem definidas ( tempo e  opções de escolha de veículo a entregar) em tempo real e centralizadas em um único lugar</li>
-                            <li>O que se espera ver num app de entrega?</li>
-                                <li>Ponto de saída, Ponto de entrega, Peso, Dimensões, Tipos de entrega, Confirmação de recebimento</li>
-                            <li>Aceitaria o uso de entrega compartilhada no caso de essa entrega demorar um pouco mais para ser realizada?</li>
-                                <li>Sim, pois certamente o valor seria mais barato e, por não trabalhar com produtos perecíveis ou cargas vivas, não teria tanta pressa e poderia planejar e  aguardar.</li>
+                    <Col xxl="9" className={styles.personaSurvey}>
+                        <ul>
+                            <li className={styles.personaQuestion}>Você utilizaria um app para receber chamados de entrega?</li>
+                                <li className={styles.personaAnswer}>Sim, hoje a maioria do serviço vem por app</li>
+                            <li className={styles.personaQuestion}>O que pesaria pra escolher trabalhar por app?</li>
+                                <li className={styles.personaAnswer}>Volume de entrega, e os ganhos, o percentual do aplicativo.</li>
+                            <li className={styles.personaQuestion}>Deixaria de fazer uma entrega de moto para fazer de bicicleta?</li>
+                                <li className={styles.personaAnswer}>Se não fosse uma distância grande e se valesse a pena financeiramente, ainda mais com o valor do combustível hoje.</li>
+                            <li className={styles.personaQuestion}>O que espera ver num app desse tipo?</li>
+                                <li className={styles.personaAnswer}>Quanto vou receber por corrida, tamanho do pacote, distância e um mapa.</li>
+                            <li className={styles.personaQuestion}>Uma empresa com responsabilidade ambiental, faz diferença na hora do profissional trabalhar?</li>
+                                <li className={styles.personaAnswer}>Acho que essa é uma questão que quem se preocupa mais é o usuário, quem vai contratar, não o trabalhador.</li>
                         </ul>
                     </Col>
                 </Row>
+                
+                    <h4 className={styles.sectionTitle}>Principais vantagens do App</h4>
+                    <Col xxl="12" xs="12">
+                        <p>
+                        Considerando as respostas dos nossos entrevistados desenvolvemos um app que apliquem-se as seguintes vantagens:
+                        </p>
+                        <ul className={styles.sectionTopicList}>
+                            <li>Centralização de serviços de courier e freteiros em um só aplicativo</li>
+                            <li>Valores acessíveis</li>
+                            <li>Incentivo a um sistema de entregas sustentável por meio de descontos</li>
+                            <li>Previsionamento de horário de entrega</li>
+                            <li>companhamento da rota de entrega</li>
+                            <li>A empresa se compromete a plantar uma muda de árvore em área de reflorestamento para cada entrega realizada por bicicleteiro ou entrega compartilhada</li>
+                            <li>Curva de aprendizado rápida, pois ele é baseado nos principais apps de mobilidade urbana do mercado</li>
+                        </ul>
+                    </Col>
+                    
             </Row>
 
-            
+            <Row className={styles.sectionDivision}>
+                <h4 className={styles.sectionTitle}>Planejamento e Wireframes</h4>
+
+                <p>
+                    As entrevistas com possíveis usuários e com os stakeholders gerou o backlog de projeto, com o intuito de basear a criação em metodologia scrum:
+                </p>
+                <br/>
+                <br/>
+
+                <Col xxl="4" xs="12">
+                <h6 className={styles.sectionTopic}>Para Sprint 1 (MVP Versão User):</h6>
+                    <ul className={styles.sectionTopicList}>
+                        <li>Registro de origem</li>
+                        <li>Registro de destino</li>
+                        <li>Seleção do meio de entrega</li>
+                        <li>Desconto por entrega compartilhada ou bicicleta</li>
+                        <li>Registro de deslocamento para busca</li>
+                        <li>Registro de deslocamento para entrega</li>
+                        <li>Confirmação de recebimento</li>
+                        <li>Avaliação do entregador</li>
+                        <li>Token de plantação</li>
+                    </ul>
+                </Col>
+                <Col xxl="4">
+                   <h6 className={styles.sectionTopic}>Para Sprint 2:</h6>
+                   <ul className={styles.sectionTopicList}>
+                        <li>Cadastro do tamanho do pacote</li>
+                        <li>Cadastro do peso do pacote</li>
+                        <li>Filtro de veículo pelo tamanho do pacote</li>
+                        <li>Histórico de pedidos</li>
+                        <li>Mapa do campo de plantação</li>                       
+                    </ul>
+                </Col>
+                <Col xxl="4">
+                   <h6 className={styles.sectionTopic}>Para Sprint 3:</h6>
+                   <ul className={styles.sectionTopicList}>
+                        <li>Foto do comprovante de entrega</li>
+                        <li>Duplicação de rota</li>
+                        <li>Entregas com retorno (com pagamento por exemplo)</li>
+                        <li>Foto do comprovante de entrega</li>
+                        <li>Busca do token de plantação</li>
+                        <li>Informações de saúde do item plantado</li>                        
+                    </ul>
+                </Col>
+
+
+                <h5 className={styles.sectionSubtitle}>Wireframes</h5>
+                <p>
+                    O fluxo desenhado em wireframes:
+                </p>
+                
+            </Row>
+
                 <Row className={styles.breakline}></Row>
                 
                 <h3 className="sectionTitle">
