5e6d789c10503585a3c7a289b61d69410df1f772
Milton Neto <leonardi.milton@gmail.com>
2026-04-17 16:06:35 -0300

    Tradução SideNav

--- components/layouts/SideNav.tsx
+++ components/layouts/SideNav.tsx
@@ -19,35 +19,35 @@ export default function SideNav(){
             
             <NavItem className={styles.menuItem} >
                 <NavLink href="./#ItsMe">
-                    <Tippy content="Who I am" placement="right" animation="perspective-extreme" className={styles.tippyBox} >
+                    <Tippy content="Quem sou?" placement="right" animation="perspective-extreme" className={styles.tippyBox} >
                         <Image src="/images/icones/alien-outline.png" alt="Who I am"  width="35" height="35" />
                     </Tippy>
                 </NavLink>
             </NavItem>
             <NavItem className={styles.menuItem}>
                 <NavLink href="./#WhatIDo">
-                    <Tippy content="My Portfolio" placement="right" animation="perspective-extreme" className={styles.tippyBox} >
+                    <Tippy content="Meu Portfolio" placement="right" animation="perspective-extreme" className={styles.tippyBox} >
                         <Image src="/images/icones/plan-outline.png" alt="What I do"  width="35" height="35"/>
                     </Tippy>
                 </NavLink>
             </NavItem>
             <NavItem className={styles.menuItem}>
                 <NavLink href="./#HowTo">
-                    <Tippy content="My skills" placement="right" animation="perspective-extreme" className={styles.tippyBox} >
+                    <Tippy content="Como eu faço" placement="right" animation="perspective-extreme" className={styles.tippyBox} >
                         <Image src="/images/icones/writing-outline.png" alt="My skills and academic formation"  width="35" height="35" className={styles.menuItemImage}/>
                     </Tippy>
                 </NavLink>
             </NavItem>
             <NavItem className={styles.menuItem}>
                 <NavLink href="./#resume">
-                    <Tippy content="Where I worked" placement="right" animation="perspective-extreme" className={styles.tippyBox} >
+                    <Tippy content="Por onde eu passei" placement="right" animation="perspective-extreme" className={styles.tippyBox} >
                         <Image src="/images/icones/location-outline.png" alt="My professional way"  width="35" height="35" />
                     </Tippy>
                 </NavLink>
             </NavItem>
             <NavItem className={styles.menuItem}>
                 <NavLink href="./#TalkToMe">
-                    <Tippy content="Let's chat" placement="right" animation="perspective-extreme" className={styles.tippyBox} >
+                    <Tippy content="Fale comigo" placement="right" animation="perspective-extreme" className={styles.tippyBox} >
                         <Image src="/images/icones/conversation-outline.png" alt="Wanna talk with me? Let's chat."  width="35" height="35"/>
                     </Tippy>
                 </NavLink>
